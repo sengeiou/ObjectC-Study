aa491af2e4482c52ab018bd0ff0a1df580e82784
Evan <1370204201@qq.com>
2015-05-16 11:30:18 +0800

    toy code commit
    toy code commit

---
+++ prog4.5/prog4.5/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  prog4.5
+//
+//  Created by YanTian Yu on 15/5/16.
+//  Copyright (c) 2015年 YanTian Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        float F = 27;
+        float C = (F -32) /1.8;
+        NSLog(@"%f", C);
+    }
+    return 0;
+}
