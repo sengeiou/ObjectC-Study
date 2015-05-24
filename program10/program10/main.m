70efc752accd9bc43ad0f3b2f61e2b1aa10849c7
Evan <1370204201@qq.com>
2015-05-24 20:16:52 +0800

    toy code commit

---
+++ program10/program10/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  program10
+//
+//  Created by YanTian Yu on 15/5/24.
+//  Copyright (c) 2015年 YanTian Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
