39c340528ad1bace7725c17e9ea5bdbf62d2aa1c
Evan Yan <1370204201@qq.com>
2015-10-11 19:11:28 +0800

    crazydragCreate

---
+++ CrazyDrag/CrazyDrag/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrazyDrag
+//
+//  Created by YanTian Yu on 15/10/11.
+//  Copyright © 2015年 ioslearning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrazyDrag/CrazyDrag/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrazyDrag
+//
+//  Created by YanTian Yu on 15/10/11.
+//  Copyright © 2015年 ioslearning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
